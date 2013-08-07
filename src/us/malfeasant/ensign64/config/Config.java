7cc7529da84a09c8914cdcc7d6f07159c6fb1ce6
Mischa Sumrall <madchao@yahoo.com>
2013-08-06 17:04:57 -0700

    more config dialog- i already don't like this, too kludgy...

--- src/us/malfeasant/ensign64/config/Config.java
+++ src/us/malfeasant/ensign64/config/Config.java
@@ -7,6 +7,8 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -34,29 +36,41 @@ public class Config implements Serializable {
 		return null;
 	}
 	
-	public void showEditor(Stage owner, Runnable onComplete) {
-		Stage stage = new Stage();
+	public boolean showEditor(Stage owner, Runnable onComplete) {
+		final Stage stage = new Stage();
 		stage.setTitle(name.equals("") ? "New machine" : name + " - Settings");
 		stage.initModality(Modality.WINDOW_MODAL);
 		stage.initOwner(owner);
 		stage.initStyle(StageStyle.UTILITY);
 		
-		ChoiceBox<Oscillator> oscChoice = new ChoiceBox<>();
+		final Label nameLabel = new Label("Name:");
+		TextField nameField = new TextField();
+		final ChoiceBox<Oscillator> oscChoice = new ChoiceBox<>();
 		oscChoice.getItems().addAll(Oscillator.values());
-		ChoiceBox<Powerline> powChoice = new ChoiceBox<>();
+		final ChoiceBox<Powerline> powChoice = new ChoiceBox<>();
 		powChoice.getItems().addAll(Powerline.values());
-		ChoiceBox<VicRev> vicChoice = new ChoiceBox<>();
+		final ChoiceBox<VicRev> vicChoice = new ChoiceBox<>();
 		vicChoice.getItems().addAll(VicRev.values());
 		
 		Button ok = new Button("Accept");
 		ok.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent arg0) {
-				
+				name = nameLabel.getText();
+				osc = oscChoice.getValue();
+				pow = powChoice.getValue();
+				vicRev = vicChoice.getValue();
+				stage.close();
 			}
 		});
 		Button cancel = new Button("Cancel");
-		
-		stage.show();
+		cancel.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				stage.close();
+			}
+		});
+		stage.showAndWait();
+		return name != null && osc != null && pow != null && vicRev != null;
 	}
 }
