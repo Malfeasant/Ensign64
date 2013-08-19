406609e7dff5668b367527c272842ebdfc11a2d1
Mischa Sumrall <madchao@yahoo.com>
2013-08-19 07:22:00 -0700

    stop

--- src/us/malfeasant/ensign64/Machine.java
+++ src/us/malfeasant/ensign64/Machine.java
@@ -1,9 +1,13 @@
 package us.malfeasant.ensign64;
 
 import us.malfeasant.ensign64.config.PixelClockRate;
+import us.malfeasant.ensign64.config.PowerRate;
+import us.malfeasant.ensign64.config.Stop;
 
 public class Machine {
-	private PixelClockRate pixelPeriod;
+	private PixelClockRate pixelRate;
+	private PowerRate powerRate;
+	
 	private int cycleCount;
 	private long totalCycles;
 	
@@ -11,5 +15,12 @@ public class Machine {
 		
 	}
 	
-	
+	/**
+	 * makes the machine run some amount of time
+	 * @param stop - when to stop running
+	 * @return how much time elapsed
+	 */
+	public double runTo(Stop stop) {
+		return 0;
+	}
 }
---
+++ src/us/malfeasant/ensign64/config/Stop.java
@@ -0,0 +1,5 @@
+package us.malfeasant.ensign64.config;
+
+public enum Stop {
+	PIXEL, CYCLE, LINE, FRAME;
+}
