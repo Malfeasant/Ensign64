fffc31918e22ba102d2bb901be66b06c744c7929
Mischa Sumrall <madchao@yahoo.com>
2014-07-24 17:56:42 -0700

    nothing much

---
+++ src/us/malfeasant/ensign64/Launcher.java
@@ -0,0 +1,13 @@
+package us.malfeasant.ensign64;
+
+import javax.swing.SwingUtilities;
+
+public class Launcher {
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> launch(args));
+	}
+	
+	private static void launch(String[] args) {
+		
+	}
+}
