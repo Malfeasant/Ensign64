cfd9c693c73ac0a0fca4d5be370f79c5021a8b25
Malfeasant <mischa.sumrall@gmail.com>
2019-07-23 22:59:26 -0700

    Quitting time again

--- src/ui/Launch.java
+++ src/ui/Launch.java
@@ -6,6 +6,7 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.image.WritableImage;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelWriter;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -23,6 +24,7 @@ public class Launch extends Application {
 		
 		BorderPane pane = new BorderPane();
 		WritableImage screenImage = new WritableImage(flavor.cycles * 8, flavor.lines);
+		PixelWriter pixWriter = screenImage.getPixelWriter();
 		ImageView view = new ImageView(screenImage);
 		view.setViewport(new Rectangle2D(0, 0, flavor.cycles * 8, flavor.lines));	// TODO: crop image
 		pane.setCenter(view);
---
+++ src/ui/Screen.java
@@ -0,0 +1,11 @@
+package ui;
+
+import javafx.scene.image.PixelWriter;
+
+class Screen {
+	private final PixelWriter pixels;
+	
+	Screen(PixelWriter p) {
+		pixels = p;
+	}
+}
